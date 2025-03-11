c114163d0e02f853e97dc666453ba4e4d4a71256
anchenspp <hellomypastor@gmail.com>
2025-03-11 21:19:17 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -14,7 +14,16 @@ const data: ISiteMetadataResult = {
   siteUrl: '',
   logo: 'https://avatars.githubusercontent.com/u/8546109?v=4',
   description: 'Personal site and blog',
-  navLinks: [],
+  navLinks: [
+    {
+      name: 'Blog', #navigation name
+      url: 'https://anchen.me/',
+    },
+    {
+      name: 'About',
+      url: 'https://anchen.me/about/',
+    },
+  ],
 };
 
 export default data;
